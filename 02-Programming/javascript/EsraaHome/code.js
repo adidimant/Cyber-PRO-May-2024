7679ae74cedb9a2c1e12ffe56bb329314734f402
Esraa <Esraa>
2024-09-09 16:33:40 +0300

    add css & js changes

---
+++ 02-Programming/javascript/EsraaHome/code.js
@@ -0,0 +1,33 @@
+
+const inputField = document.getElementById("colorInput");
+const colorBox = document.getElementById("colorBox");
+
+
+inputField.addEventListener("keydown", function() {
+colorBox.style.backgroundColor = "#6495ED"; 
+});
+
+
+inputField.addEventListener("keyup", function() {
+colorBox.style.backgroundColor = "#DE3163";
+});
+
+
+
+
+
+
+
+function Form() 
+{
+    document.getElementById("Fname").value =" Esraa";
+    document.getElementById("Lname").value =" Mo";
+    document.getElementById("email").value =" exampelgmail.com";
+
+
+}
+
+
+
+
+
