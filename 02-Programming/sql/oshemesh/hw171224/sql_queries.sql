f01e7bba637ae2aa33eef08734a25ed934f457e6
oshemesh <oshemesh73@gmail.com>
2024-12-20 09:16:41 +0200

    sq lhw 171224

---
+++ 02-Programming/sql/oshemesh/hw171224/sql_queries.sql
@@ -0,0 +1,29 @@
+--1) Get all the orders that the order deal is above 300$
+
+SELECT item FROM Orders Where amount >300;
+
+--2) Get all the unique item name from the orders
+
+SELECT DISTINCT item FROM Orders;
+
+--3) Get all the unique items of specific customer ID = 4
+SELECT DISTINCT item FROM Orders WHERE customer_id=4;
+
+--4) get how many shippings are in "Pending" status
+SELECT Count(shipping_id) FROM Shippings WHERE status="Pending";
+
+--5) Get how many customers have more than 1 item in "Pending" status
+SELECT Count(customer) FROM Shippings WHERE status="Pending" AND customer >1;
+
+--6) get all the customers that their country is USA and their age is higher than 25,
+-- OR they are from UK and their age is lower than 25
+SELECT first_name, last_name FROM Customers WHERE (country="USA" AND age >25) OR (country="UK" AND age <25);
+
+--7) Get all the customers that are from UK and their first name starts with "D..."
+SELECT first_name, last_name FROM Customers WHERE country="UK" AND first_name LIKE "D%";
+
+--8) Get the average amount of all the last orders
+SELECT AVG(amount)FROM Orders
+
+--9) get the average amount of all the last orders that are at least 300$
+SELECT AVG(amount)FROM Orders WHERE amount >= 300;
